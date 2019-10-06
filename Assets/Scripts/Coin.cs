22725a6cad6a37785ef72d0a156361b09551c4a8
Matěj Beran <matej.beran96@gmail.com>
2019-10-06 21:37:17 +0200

    Make lives and core persistent and shown to the player.

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] int points = 25;
+    [SerializeField] AudioClip soundSFX;
+
     void OnTriggerEnter2D(Collider2D other)
     {
+        AudioSource.PlayClipAtPoint(soundSFX, Camera.main.transform.position);
+        FindObjectOfType<GameSession>().AddToScore(points);
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -2,11 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playerLives = 3;
+    [SerializeField] Text livesText;
+    [SerializeField] Text scoreText;
+
+    int score = 0;
 
     void Awake()
     {
@@ -23,7 +28,8 @@ public class GameSession : MonoBehaviour
 
     void Start()
     {
-
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
     }
 
     public void ProcessPlayerDeath()
@@ -38,6 +44,12 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    public void AddToScore(int score)
+    {
+        this.score += score;
+        scoreText.text = score.ToString();
+    }
+
     private void ResetGameSession()
     {
         SceneManager.LoadScene(0);
@@ -47,6 +59,7 @@ public class GameSession : MonoBehaviour
     private void TakeLife()
     {
         playerLives--;
+        livesText.text = playerLives.ToString();
         var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
     }
